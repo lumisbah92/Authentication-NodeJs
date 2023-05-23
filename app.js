9bc49849e54e0494570ec366e54d6b4e79e74121
Misbah Uddin Tareq <lumisbah92@gmail.com>
2023-05-24 03:13:43 +0600

    added hashing passwords

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require('md5');
 
 const app = express();
 
@@ -18,8 +18,6 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String,
 });
-
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -38,7 +36,7 @@ app.get("/login", (req, res) => {
 app.post("/register", async (req, res) => {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password,
+        password: md5(req.body.password),
     });
 
     try {
@@ -53,7 +51,7 @@ app.post("/login", async (req, res) => {
     try {  
         const foundUser = await User.findOne({email: req.body.username});
         if(foundUser) {
-            if(foundUser.password === req.body.password)
+            if(foundUser.password === md5(req.body.password))
                 res.render("secrets");
         }
         else console.log("No User found");
