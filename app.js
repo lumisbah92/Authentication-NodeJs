f297bd3de075cbf706d180a4f2c224a27aeda515
Misbah Uddin Tareq <lumisbah92@gmail.com>
2023-05-28 13:28:06 +0600

    added authentication with passport-local-mongoose

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 //jshint esversion:6
 require('dotenv').config();
 const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require('md5');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const app = express();
 
 app.use(express.static("public"));
+app.use(cors());
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect("mongodb://127.0.0.1:27017/userDB");
 
@@ -19,7 +34,14 @@ const userSchema = new mongoose.Schema({
     password: String,
 });
 
+userSchema.plugin(passportLocalMongoose);
+
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", (req, res) => {
     res.render("home");
@@ -33,31 +55,52 @@ app.get("/login", (req, res) => {
     res.render("login");
 });
 
-app.post("/register", async (req, res) => {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password),
+app.get("/logout", (req, res, next) => {
+    req.logout(function (err) {
+        if (err) { return next(err); }
+        res.redirect('/');
     });
+});
 
-    try {
-        await newUser.save();
-        res.render("login");
-    } catch (error) {
-        console.log(error);
+app.get("/secrets", (req, res) => {
+    if (req.isAuthenticated()) {
+        res.render("secrets");
+    }
+    else {
+        res.redirect("/login");
     }
 });
 
-app.post("/login", async (req, res) => {
-    try {  
-        const foundUser = await User.findOne({email: req.body.username});
-        if(foundUser) {
-            if(foundUser.password === md5(req.body.password))
-                res.render("secrets");
+app.post("/register", async (req, res) => {
+    User.register({ username: req.body.username }, req.body.password, function (err, user) {
+        if (err) {
+            console.log(err);
+            res.redirect("/register");
         }
-        else console.log("No User found");
-    } catch (error) {
-        console.log(error);
-    }
+        else {
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/secrets");
+            });
+        }
+    });
+});
+
+app.post("/login", async (req, res) => {
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password,
+    });
+
+    req.login(user, function (err) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/secrets");
+            });
+        }
+    });
 });
 
 app.listen(3000, () => { console.log(`Server started on port 3000`) });
